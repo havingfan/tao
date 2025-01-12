d0408df1c21013a54e45b8746495b715a93ce1ad
have fun <376076821@qq.com>
2025-01-12 19:49:12 +0800

    tijiaowemnjan

--- ana.c
+++ ana.c
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <Math/Vector4D.h>
 int ana(){
  
   string dir = "/home/yrliu/Desktop/";
@@ -16,7 +17,7 @@ int ana(){
   vector<double> *p_status=0;
   vector<int> *p_id=0;
   int npar=0;
-  TLorentzVector L;
+  TLorentzVector PtEtaPhiEVector;
   vector<double> PE0, PE1, PE2, PPT0, PPT1, PPT2, PETA0, PETA1, PETA2, PPHI0, PPHI1, PPHI2;
   double Pt0 = 0;
   double Pt1 = 0;
@@ -59,14 +60,14 @@ int ana(){
   my_tree->SetBranchAddress("p_status", &p_status, &b_p_status );
   
   TCanvas *c11 = new TCanvas();
-  TH1I *h11 = new TH1I( "h11", "The Pt(nu_tau) distribution", 30, 0, 3);
-  TH1I *h12 = new TH1I( "h12", "The Pt(pi) distribution", 30, 0, 3);
-  TH1I *h21 = new TH1I( "h21", "The Eta(nu_tau) distribution", 40, -4, 4);
-  TH1I *h22 = new TH1I( "h22", "The Eta(pi) distribution", 40, -4, 4);
-  TH1I *h31 = new TH1I( "h31", "The Phi(nu_tau) distribution", 40, -4, 4);
-  TH1I *h32 = new TH1I( "h32", "The Phi(pi) distribution", 40, -4, 4);
-  TH1I *h41 = new TH1I( "h41", "The E(nu_tau) distribution", 35, 0, 3.5);
-  TH1I *h42 = new TH1I( "h42", "The E(pi) distribution", 35, 0, 3.5);
+  TH1I *h11 = new TH1I( "h11", "The Pt distribution of nu tau",30, 0, 3);
+  TH1I *h12 = new TH1I( "h12", "The Pt distribution of pi", 30, 0, 3);
+  TH1I *h21 = new TH1I( "h21", "The Eta distribution of nu tau", 40, -4, 4);
+  TH1I *h22 = new TH1I( "h22", "The Eta distribution of pi", 40, -4, 4);
+  TH1I *h31 = new TH1I( "h31", "The Phi distribution of nu tau", 40, -4, 4);
+  TH1I *h32 = new TH1I( "h32", "The Phi distribution of pi", 40, -4, 4);
+  TH1I *h41 = new TH1I( "h41", "The E distribution of nu tau", 35, 0, 3.5);
+  TH1I *h42 = new TH1I( "h42", "The E distribution of pi", 35, 0, 3.5);
 
 //Pick out three kinds of particles and put them in three vectors.
   N = (int) my_tree->GetEntries();
@@ -125,15 +126,17 @@ int ana(){
 			    E2 = PE2[i];
 			    sum1 = abs(Pt1 + Pt2 - Pt0) + abs(Eta1 + Eta2 - Eta0) +abs(Phi1 + Phi2 -Phi0);
 		            if (sum1 < sum){
-		                 sum = sum1;
-				 Pt11 = Pt1;
-				 Pt22 = Pt2;
-				 Eta11 = Eta1;
-				 Eta22 = Eta2;
-				 Phi11 = Phi1;
-				 Phi22 = Phi2;
-			         E11 = E1;
-			         E22 = E2;}
+				 sum = sum1;
+				 PtEtaPhiEVector.SetPtEtaPhiE(Pt1,Eta1,Phi1,E1);
+				 Pt11 = PtEtaPhiEVector.Pt();
+                                 Eta11 = PtEtaPhiEVector.Eta();
+                                 Phi11 = PtEtaPhiEVector.Phi();
+                                 E11 = PtEtaPhiEVector.E();
+				 PtEtaPhiEVector.SetPtEtaPhiE(Pt2,Eta2,Phi2,E2);
+                                 Pt22 = PtEtaPhiEVector.Pt();
+                                 Eta22 = PtEtaPhiEVector.Eta();
+                                 Phi22 = PtEtaPhiEVector.Phi();
+                                 E22 = PtEtaPhiEVector.E();}
                         }
 	           }
 	           h11->Fill(Pt11);
@@ -183,14 +186,17 @@ int ana(){
                             sum1 = abs(Pt1 + Pt2 - Pt0) + abs(Eta1 + Eta2 - Eta0) +abs(Phi1 + Phi2 -Phi0);
                             if (sum1 < sum){
                                  sum = sum1;
-                                 Pt11 = Pt1;
-                                 Pt22 = Pt2;
-                                 Eta11 = Eta1;
-                                 Eta22 = Eta2;
-                                 Phi11 = Phi1;
-                                 Phi22 = Phi2;
-                                 E11 = E1;
-                                 E22 = E2;}
+				 sum = sum1;
+                                 PtEtaPhiEVector.SetPtEtaPhiE(Pt1,Eta1,Phi1,E1);
+                                 Pt11 = PtEtaPhiEVector.Pt();
+                                 Eta11 = PtEtaPhiEVector.Eta();
+                                 Phi11 = PtEtaPhiEVector.Phi();
+                                 E11 = PtEtaPhiEVector.E();
+                                 PtEtaPhiEVector.SetPtEtaPhiE(Pt2,Eta2,Phi2,E2);
+                                 Pt22 = PtEtaPhiEVector.Pt();
+                                 Eta22 = PtEtaPhiEVector.Eta();
+                                 Phi22 = PtEtaPhiEVector.Phi();
+                                 E22 = PtEtaPhiEVector.E();}
                         }
                    }
                    h11->Fill(Pt11);
@@ -239,14 +245,17 @@ int ana(){
 			    sum1 = abs(Pt1 + Pt2 - Pt0) + abs(Eta1 + Eta2 - Eta0) +abs(Phi1 + Phi2 -Phi0);
                             if (sum1 < sum){
                                  sum = sum1;
-                                 Pt11 = Pt1;
-                                 Pt22 = Pt2;
-                                 Eta11 = Eta1;
-                                 Eta22 = Eta2;
-                                 Phi11 = Phi1;
-                                 Phi22 = Phi2;
-                                 E11 = E1;
-                                 E22 = E2;}
+				 sum = sum1;
+                                 PtEtaPhiEVector.SetPtEtaPhiE(Pt1,Eta1,Phi1,E1);
+                                 Pt11 = PtEtaPhiEVector.Pt();
+                                 Eta11 = PtEtaPhiEVector.Eta();
+                                 Phi11 = PtEtaPhiEVector.Phi();
+                                 E11 = PtEtaPhiEVector.E();
+                                 PtEtaPhiEVector.SetPtEtaPhiE(Pt2,Eta2,Phi2,E2);
+                                 Pt22 = PtEtaPhiEVector.Pt();
+                                 Eta22 = PtEtaPhiEVector.Eta();
+                                 Phi22 = PtEtaPhiEVector.Phi();
+                                 E22 = PtEtaPhiEVector.E();}
                         }
                    }
                    h11->Fill(Pt11);
@@ -281,31 +290,46 @@ int ana(){
     PETA0.clear(); PETA1.clear(); PETA2.clear();
     PPHI0.clear(); PPHI1.clear(); PPHI2.clear();
     }
-
+    h11->GetXaxis()->SetTitle("Pt");
+    h11->GetYaxis()->SetTitle("Events");
     h11->Draw();
     c11->Print("c11.png");
     cout << endl;
+    h12->GetXaxis()->SetTitle("Pt");
+    h12->GetYaxis()->SetTitle("Events");
     h12->Draw();
     c11->Print("c12.png");
     cout << endl;
+    h21->GetXaxis()->SetTitle("Eta");
+    h21->GetYaxis()->SetTitle("Events");
     h21->Draw();
     c11->Print("c21.png");
     cout << endl;
+    h22->GetXaxis()->SetTitle("Eta");
+    h22->GetYaxis()->SetTitle("Events");
     h22->Draw();
     c11->Print("c22.png");
     cout << endl;
+    h31->GetXaxis()->SetTitle("Phi");
+    h31->GetYaxis()->SetTitle("Events");
     h31->Draw();
     c11->Print("c31.png");
     cout << endl;
+    h32->GetXaxis()->SetTitle("Phi");
+    h32->GetYaxis()->SetTitle("Events");
     h32->Draw();
     c11->Print("c32.png");
     cout << endl;
+    h41->GetXaxis()->SetTitle("E");
+    h41->GetYaxis()->SetTitle("Events");
     h41->Draw();
     c11->Print("c41.png");
     cout << endl;
+    h42->GetXaxis()->SetTitle("E");
+    h42->GetYaxis()->SetTitle("Events");
     h42->Draw();
     c11->Print("c42.png");
     cout << endl;
-
+   
   return 0;
 }
